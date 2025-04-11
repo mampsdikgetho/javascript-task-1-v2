623a14752e5e32f4fc282b997e9950dac03aa2b1
mampsdikgetho <dikgethomamps@gmail.com>
2025-04-11 08:29:54 +0200

    initial commit

---
+++ datatypes_operators.js
@@ -0,0 +1,24 @@
+let integerNum = 7;
+let floatNumber = 7.5;
+console.log(integerNum + 3, floatNum - 2, integerNum * 2, floatNum / 2, integerNum % 3, integerNum ** 2);
+
+
+let x 8, y = 12;
+console.log(x > y, x <= y, x===y, x !==y);
+
+let a = true, b = false;
+console.log(a && b, a || b, !a);
+
+let p = 10;
+p += 5;
+console.log(p);
+p -= 2;
+console.log(p)
+p *=3;
+console.log(p);
+p /= 2;
+console.log(p);
+p %= 4;
+console.log(p);
+
+
---
+++ variable.js
@@ -0,0 +1,20 @@
+let name= "Dikgetho Mampane";
+let age= 26;
+let isStudent = true; 
+console.log(Name);
+console.log(age);
+console(isStudent);
+
+
+let favouriteColour = "Blue";
+console.log(favouriteColour);
+favouriteColour ="Green";
+console.log(favouriteColour);
+
+let num1 = 10;
+let num2 = 5
+let sum = num1 = num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+console.log(sum, difference, product , quotient);
